7498934fd9d12adf4e4c17d2c29d784539d6c758
daveM <david.meredith@stfc.ac.uk>
2023-02-03 11:55:03 +0000

    Keymap bugfix for <C-p> to open FZF

--- init.lua
+++ init.lua
@@ -341,11 +341,30 @@ require('gitsigns').setup {
       vim.schedule(function() gs.prev_hunk() end)
       return '<Ignore>'
     end, {expr=true})
-    
+
+    -- Gitsigns Actions 
+    -- see: https://github.com/omerxx/dotfiles/blob/master/nvim/init.lua 
+    -- and: https://www.youtube.com/watch?v=IyBAuDPzdFY&list=WL&index=2  
+    -- map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    -- map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    -- map('n', '<leader>hS', gs.stage_buffer)
+    -- map('n', '<leader>ha', gs.stage_hunk)
+    -- map('n', '<leader>hu', gs.undo_stage_hunk)
+    -- map('n', '<leader>hR', gs.reset_buffer)
+    -- map('n', '<leader>hp', gs.preview_hunk)
+    -- map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    -- map('n', '<leader>tb', gs.toggle_current_line_blame)
+    -- map('n', '<leader>hd', gs.diffthis)
+    -- map('n', '<leader>hD', function() gs.diffthis('~') end)
+    -- map('n', '<leader>td', gs.toggle_deleted)
+    -- -- Text object
+    -- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+
   end
 }
 
 -- [[ Configure Telescope ]]
+-- :Telescope <args>
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
